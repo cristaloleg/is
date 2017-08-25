020992a4cc4cb64afa5c9309ecff53bf2ddf9fb7
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-08-25 19:51:12 +0200

    a lot of stubs, Err, NoErr impl

--- is.go
+++ is.go
@@ -1,23 +1,46 @@
 package is
 
-import (
-	"testing"
-)
-
-// Is ...
+// Is is a main interface in this lib
 type Is interface {
 	Nil(interface{})
 	OK(interface{})
+	Err(error)
+	NoErr(error)
+
+	Type(interface{})
+	Impl(interface{})
+	Match(string, interface{})
+	Pos(interface{})
+	Neg(interface{})
+	Zero(interface{})
+	Int(interface{})
+	Float(interface{})
+	NaN(interface{})
+	Empty(interface{})
+	Closed(interface{})
+	Filled(interface{})
+	Contains(interface{}, interface{})
+	Equal(interface{}, interface{})
+	NotEqual(interface{}, interface{})
+}
+
+// T interface for testing.T
+type T interface {
+	Log(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fail()
+	FailNow()
+	Helper()
 }
 
 type is struct {
-	t        *testing.T
+	t        T
 	isStrict bool
 	text     string
 }
 
 // New ...
-func New(t *testing.T) Is {
+func New(t T) Is {
 	is := &is{
 		t:        t,
 		isStrict: false,
@@ -26,7 +49,7 @@ func New(t *testing.T) Is {
 }
 
 // NewStrict ...
-func NewStrict(t *testing.T) Is {
+func NewStrict(t T) Is {
 	is := &is{
 		t:        t,
 		isStrict: true,
@@ -35,30 +58,64 @@ func NewStrict(t *testing.T) Is {
 }
 
 func (i *is) fail() {
-	i.t.Logf(i.text)
+	i.t.Log(i.text)
 	i.t.FailNow()
 }
 
 // Nil ...
-func (i *is) Nil(object interface{}) {
+func (i *is) Nil(obj interface{}) {
 	i.t.Helper()
-	if object == nil {
+	if obj == nil {
 		return
 	}
-	i.t.Errorf("expected nil %v", object)
+	i.t.Errorf("expected nil, but got %v", obj)
 	if i.isStrict {
 		i.fail()
 	}
 }
 
 // Nil ...
-func (i *is) OK(object interface{}) {
+func (i *is) OK(obj interface{}) {
 	i.t.Helper()
-	if object != nil {
+	if obj != nil {
 		return
 	}
-	i.t.Errorf("expected not nil %v", object)
+	i.t.Errorf("expected not nil")
 	if i.isStrict {
 		i.fail()
 	}
 }
+
+func (i *is) Err(err error) {
+	if err != nil {
+		return
+	}
+	i.t.Errorf("expected error, but got nil")
+	if i.isStrict {
+		i.fail()
+	}
+}
+func (i *is) NoErr(err error) {
+	if err == nil {
+		return
+	}
+	i.t.Errorf("expected nil, but got %v", err)
+	if i.isStrict {
+		i.fail()
+	}
+}
+func (i *is) Type(interface{})                  {}
+func (i *is) Impl(interface{})                  {}
+func (i *is) Match(string, interface{})         {}
+func (i *is) Pos(interface{})                   {}
+func (i *is) Neg(interface{})                   {}
+func (i *is) Zero(interface{})                  {}
+func (i *is) Int(interface{})                   {}
+func (i *is) Float(interface{})                 {}
+func (i *is) NaN(interface{})                   {}
+func (i *is) Empty(interface{})                 {}
+func (i *is) Closed(interface{})                {}
+func (i *is) Filled(interface{})                {}
+func (i *is) Contains(interface{}, interface{}) {}
+func (i *is) Equal(interface{}, interface{})    {}
+func (i *is) NotEqual(interface{}, interface{}) {}
